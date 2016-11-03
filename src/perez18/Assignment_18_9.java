8ec8c9c3e62952d333210d12dd0ab3da367c34db
mperez5 <mperez154@gmail.com>
2016-11-03 18:09:47 -0400

    Deleted 18.21, Updated Exercise, Formatted code

--- src/perez18/Assignment_18_9.java
+++ src/perez18/Assignment_18_9.java
@@ -9,40 +9,34 @@ public class Assignment_18_9 {
 		Scanner input = new Scanner(System.in);
 		System.out.println("Enter an integer");
 		String myInteger = input.nextLine();
-				
-		//Calling recursive method
-		reverseDisplay(myInteger);	
-		
+
+		// Calling recursive method
+		reverseDisplay(myInteger);
+
 		input.close();
 
 	}
-	
-	public static void reverseDisplay(String integer)
-	{			
-		if(integer.length() == 0)
-		{
+
+	public static void reverseDisplay(String integer) {
+		if (integer.length() == 0) {
 			System.out.println("\nCompleted");
-		}
-		else if(integer.length() == 1)
-		{
+		} else if (integer.length() == 1) {
 			System.out.println(integer);
-		}
-		else
-		{
-			System.out.print(integer.charAt(integer.length()-1));	
-			integer = integer.substring(0, integer.length()-1);		
+		} else {
+			System.out.print(integer.charAt(integer.length() - 1));
+			integer = integer.substring(0, integer.length() - 1);
 			reverseDisplay(integer);
 		}
 	}
 }
 
-
-/*	Assignment Details: 
- 
- 	(Print the digits in an integer reversely) Write a recursive method that displays
-	an int value reversely on the console using the following header:
-	public static void reverseDisplay(int value)
-	For example, reverseDisplay(12345) displays 54321. Write a test program
-	that prompts the user to enter an integer and displays its reversal.
-	
-*/
+/*
+ * Assignment Details:
+ * 
+ * (Print the digits in an integer reversely) Write a recursive method that
+ * displays an int value reversely on the console using the following header:
+ * public static void reverseDisplay(int value) For example,
+ * reverseDisplay(12345) displays 54321. Write a test program that prompts the
+ * user to enter an integer and displays its reversal.
+ * 
+ */
